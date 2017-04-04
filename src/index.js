9f0ecab82b73d58932aea61391913ed76a72cba2
Maarten Hus <maarten.hus@42.nl>
2017-04-04 14:56:47 +0200

    Bugfix: the FlashMessageStore type is exported now.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export { configureFlashMessages, getConfig } from './config.js';
 export { addError, addWarning, addSuccess, addInfo } from './service.js';
 export { flashMessage, addFlashMessage, removeFlashMessage } from './flash-message-reducer.js';
 
+export type { FlashMessageStore } from './flash-message-reducer.js';
 export type { FlashMessage, FlashMessageConfig, OnFlashMessageClicked } from './models.js';
